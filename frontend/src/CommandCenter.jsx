f2713441f32a70eb5a7ed76bc028d59089a8c906
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-17 17:09:20 -0500

    finsished soem styleing

--- frontend/src/CommandCenter.jsx
+++ frontend/src/CommandCenter.jsx
@@ -1,8 +1,13 @@
 function CommandCenter() {
   return (
     <main className="command-center">
-      <div className="title1">
-        <h1>Command Center</h1>
+        <div className="title1">
+            <h1>Command Center</h1>
+        </div>
+        <div className="todays-code">
+            <h1>CODED HOURS</h1>
+            <p>target: taget here</p>
+            <h1>CODE HOURS / target</h1>
         </div>
       
     </main>
