8fd47c7c55f4b9d45953ef9ffed356bc6fbe2457
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-25 22:29:35 -0500

    pushing the progress so far

--- frontend/src/ProjectBreakdown.jsx
+++ frontend/src/ProjectBreakdown.jsx
@@ -1,6 +1,12 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate} from "react-router-dom"
 
 function ProjectBreakdown() {
+
+    const [selectedProject, setSelectedProject] = useState("")
+    const navigate = useNavigate()
+    const [projects, setProjects] = useState([])
+
     return (
         <main>
             <h1>Project Breakdown</h1>
@@ -13,7 +19,33 @@ function ProjectBreakdown() {
             <p>Most Recent Project: </p>
             </section>
 
-            
+            <section className="project-story">
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault()
+
+                    if (selectedProject) {
+                        navigate(`/project-story/${encodeURIComponent(selectedProject)}`)
+                    }
+                }}
+                    >
+                <select
+                    value={selectedProject}
+                    onChange={(event) => setSelectedProject(event.target.value)}
+                    required
+                >
+                    <option value="" disabled>Choose a project</option>
+
+                    {projects.map((project) => (
+                    <option key={project.name} value={project.name}>
+                        {project.name}
+                    </option>
+                    ))}
+                </select>
+
+                <button type="submit" onClick={}>View story →</button>
+                </form>
+            </section>
         </main>
     )
 }
